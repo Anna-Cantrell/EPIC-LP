d3625d98b77e1493db3b0bebb714c2a485ac7b0a
anna-cantrell <dcxdesigns@gmail.com>
2017-08-06 17:28:56 -0400

    adds the form and interactive fields, as well as menu functionality. jquery also added.

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,34 @@
  * This document contains all of the custom scripts for website. This is written
  * using jQuery to simplify code complexity.
  */
+var clicks = 0;
+var ctaButton = document.getElementById('#cta');
+var toggle = document.getElementById('toggle');
+
+$('#cta').click(function() {
+    $('#form-holder').addClass('form-open');
+    $('#form-holder').addClass('form-open');
+    $('#menuCircle').addClass('toggle-circle-active');
+    $('#line1').addClass('toggle-line1-active');
+    $('#line2').addClass('toggle-line2-active');
+    $('#line3').addClass('toggle-line3-active');
+    clicks = 1;
+});
+$('#toggle').click(function() {
+    clicks ++;
+    if (clicks % 2 != 0 ) {
+        $('#form-holder').addClass('form-open');
+        $('#menuCircle').addClass('toggle-circle-active');
+        $('#line1').addClass('toggle-line1-active');
+        $('#line2').addClass('toggle-line2-active');
+        $('#line3').addClass('toggle-line3-active');
+    } else {
+        $('#form-holder').removeClass('form-open');
+        $('#menuCircle').removeClass('toggle-circle-active');
+        $('#line1').removeClass('toggle-line1-active');
+        $('#line2').removeClass('toggle-line2-active');
+        $('#line3').removeClass('toggle-line3-active');
+    }
+})
+
+
